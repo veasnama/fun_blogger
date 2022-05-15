931dc5eafacd9fb1148bb40bf9769bd6312d12fe
veasnama <maveasnalearntosolve@gmail.com>
2022-05-15 23:32:18 +0700

    added django template, views, use render()

--- fun_blogger/blogpost/urls.py
+++ fun_blogger/blogpost/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
         path('', views.index, name='index'),
+        path('<int:blog_id>/', views.details, name='details'),
+        path('<int:blog_id>/comments/', views.comments, name='comments'),
+        path('home_page/', views.home_page, name='home_page'),
 ]
-
-
--- fun_blogger/blogpost/views.py
+++ fun_blogger/blogpost/views.py
@@ -2,6 +2,25 @@
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import User
 def index(request):
-    return HttpResponse("Hello, World From Django project.")
+    latest_added_users = User.objects.order_by('-pub_date')[:3] 
+    template = loader.get_template('blogpost/index.html')
+    context = {
+
+            'latest_added_users': latest_added_users,
+    }
+    return HttpResponse(template.render(context, request))
+
+def home_page(request):
+    respone =  "What are you looking at now , this is Djanog HomePage"
+    return HttpResponse(respone)
+
+def details(request, blog_id):
+    return HttpResponse("You're looking at blog  %s." % blog_id)
+
+def comments(request, blog_id):
+    return HttpResponse("You're commenting at the blog %s." % blog_id)
+
+
