4321065fa993a9e2727c1f1cfd8d69f15a7c8dd8
veasnama <maveasnalearntosolve@gmail.com>
2022-05-15 17:04:04 +0700

    add database model and create user

--- fun_blogger/blogpost/models.py
+++ fun_blogger/blogpost/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class User(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=200)
+    user_role = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('date created')
+
+    def __str__(self):
+        return self.username
+
+class Blog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.CharField(max_length=100)
+    tilte = models.CharField(max_length=1000)
+    content = models.CharField(max_length=15000)
+    created_at = models.DateTimeField('date created')
+    pub_date = models.CharField(max_length=100)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.owner 
+    
+
